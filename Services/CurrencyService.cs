991f755ef0028ca8cb9ffd6f40867a8594bd36d9
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2024-02-05 17:19:20 +0100

    Create (I)CurrencyService
    This service handles bills and coins

---
+++ Services/CurrencyService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace SelfCheckoutMachine.Services
+{
+    /// <summary>
+    /// Responsible for:
+    ///     Storing bills and coins
+    ///     Handling checkouts
+    /// </summary>
+    public class CurrencyService : ICurrencyService
+    {
+        public IDictionary<string, decimal> Checkout(IDictionary<string, decimal> inserted, decimal price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, decimal> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, decimal> Store(IDictionary<string, decimal> inserted)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ICurrencyService.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCheckoutMachine.Services
+{
+    public interface ICurrencyService
+    {
+        // Responsible for storing the inserted bills and coins in the machine
+        IDictionary<string, decimal> Store(IDictionary<string, decimal> inserted);
+
+        // Returns available bills and coins stored in the machine
+        IDictionary<string, decimal> List();
+
+        // Handles checkouts, updates available denominations
+        IDictionary<string, decimal> Checkout(IDictionary<string, decimal> inserted, decimal price);
+    }
+}
