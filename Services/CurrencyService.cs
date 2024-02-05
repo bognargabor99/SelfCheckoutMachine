cf862d355cf73c0ede55ed6bfb69dceaa4117640
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2024-02-05 17:28:46 +0100

    Implement listing stored denominations in CurrencyService

--- Services/CurrencyService.cs
+++ Services/CurrencyService.cs
@@ -9,6 +9,17 @@ namespace SelfCheckoutMachine.Services
     /// </summary>
     public class CurrencyService : ICurrencyService
     {
+        private readonly List<string> AcceptedDenominations = ["5", "10", "20", "50", "100", "200", "500", "1000", "2000", "5000", "10000", "20000"];
+
+        private readonly ConcurrentDictionary<string, decimal> _currencies = new();
+
+        public CurrencyService()
+        {
+            // Initializing stored amounts to zero (no money is stored in the beginning)
+            foreach (var currency in AcceptedDenominations)
+                _currencies[currency] = 0;
+        }
+
         public IDictionary<string, decimal> Checkout(IDictionary<string, decimal> inserted, decimal price)
         {
             throw new NotImplementedException();
@@ -16,7 +27,7 @@ namespace SelfCheckoutMachine.Services
 
         public IDictionary<string, decimal> List()
         {
-            throw new NotImplementedException();
+            return _currencies;
         }
 
         public IDictionary<string, decimal> Store(IDictionary<string, decimal> inserted)
