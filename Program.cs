d50af71593c0abc92f36cad841b99e86916d386e
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2024-02-05 17:27:10 +0100

    Dependency injection for ICurrencyService

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using SelfCheckoutMachine.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<ICurrencyService, CurrencyService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
