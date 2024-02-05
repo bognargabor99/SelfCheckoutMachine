5d00b0a3166f15d1b86c3286b5c77572faded1f4
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2024-02-06 00:09:25 +0100

    Add API endpoint for Checkout

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelfCheckoutMachine.Model.DTO;
+using SelfCheckoutMachine.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SelfCheckoutMachine.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CheckoutController(ICheckoutService _checkoutService) : ControllerBase
+    {
+        private readonly ICheckoutService checkoutService = _checkoutService;
+
+        [HttpPost]
+        public IActionResult Checkout([FromBody] CheckoutDTO checkoutDetails)
+        {
+            try
+            {
+                return this.Ok(this.checkoutService.Checkout(checkoutDetails.Inserted, checkoutDetails.Price));
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ Model/DTO/CheckoutDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SelfCheckoutMachine.Model.DTO
+{
+    public class CheckoutDTO
+    {
+        [JsonPropertyName("inserted")]
+        public IDictionary<string, uint> Inserted { get; set; } = null!;
+
+        [JsonPropertyName("price")]
+        public uint Price { get; set; }
+    }
+}
