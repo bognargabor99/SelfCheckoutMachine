8e24cac04547f78c461099a8f375066c337e4b7b
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2024-02-07 16:58:49 +0100

    Add model class for db

---
+++ Model/Denomination.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SelfCheckoutMachine.Model
+{
+    public class Denomination
+    {
+        [Key]
+        public required string Value { get; set; }
+
+        public uint Amount { get; set; }
+    }
+}
